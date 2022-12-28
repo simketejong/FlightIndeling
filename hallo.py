9ceb431d00e0fa1bac967f905b75ba200c453528
simketejong <simketejong@gmail.com>
2022-12-28 16:50:24 +0100

    Zoeken na kandidaat, en update dictionary

--- hallo.py
+++ hallo.py
@@ -1,29 +1,79 @@
-print("Hallo World")
+import random
+
 AantalPersonen = 10
-y = [1,2,3,4,5]
-fruit = []
-fruit.append("een")
-fruit.append(2)
-fruit.append("drie")
-fruit.append(y)
+AantalSpeelDagen = 4
+FlightsPerDag = 4
+MaximaleFlightGrote = 4
+MinimaleFlightGrote = 3
+
 Persoon =[]
-print("Dit is mijn eerste variabel")
-print("Dus alles wat ik hier doet komt rechts")
-for x in fruit:
-    print(x)
+FlightIndeling =[]
+TestRemain = AantalPersonen % MaximaleFlightGrote
+TestDevide = TestRemain / MinimaleFlightGrote
+
+FlightVerdeling=[4,4,3,3] # testing
 
 J = { 
     "nummer" : 1,
     "naam" : "",
-    "leeftijd" : "",
-    "met_wie_gespeeld" : []
+    "hcp" : "",
+    "email" : "",
+    "met_wie_gespeeld" : [],
+    "WelkeFlight" : [],
+    "WelkeDag" : []
 }
 
+P = {
+    "Dag" : 0,
+    "Flight" : 0,
+    "Grote" : 0,
+    "Personen" : []
+}
+
+
+def WieOhWie(Dag,lid,nummer, Grote):
+    Dag=Dag+1
+    FlightIndeling.append(P)
+    FlightIndeling[Dag].update({"Dag": Dag})
+    FlightIndeling[Dag].update({"Flight": nummer})
+    FlightIndeling[Dag].update({"Grote": Grote})
+    for i in range(Grote):
+        GaDoor = True
+        Tijd = 1001
+        while GaDoor:
+            Kandidaat = random.randint(0, 9)            
+            if not Dag in Persoon[Kandidaat]["WelkeDag"]:
+                Tijd = Tijd - 1
+                if Tijd <= 0:
+                    GaDoor = False
+                    print("Kandidaat : ", Kandidaat, "niet gelukt in " , FlightIndeling[Dag])
+                if not Kandidaat in Persoon[Kandidaat]["met_wie_gespeeld"]:
+                    Persoon[Kandidaat]["met_wie_gespeeld"].append(Kandidaat)
+                    Persoon[Kandidaat]["WelkeFlight"].append([nummer])
+                    Persoon[Kandidaat]["WelkeDag"].append([Dag])
+                if not Kandidaat in FlightIndeling[Dag]["Personen"]:
+                    FlightIndeling[Dag]["Personen"].append(Kandidaat)
+                    GaDoor = False
+                #print("Kandidaat : ",Kandidaat)
+
+
+FlightIndeling.append(P)
 Persoon.append(J)
 for x in range(AantalPersonen):
     Persoon.append(J)
     Persoon[x].update({"nummer": x})
-    print("Persoon 0 = ",Persoon[x])
+    Persoon[x].update({"naam": x+1})
+#    print("Persoon = ",Persoon[x])
+
+for Dag in range(AantalSpeelDagen ):
+    nummer = 0    
+    for Grote in FlightVerdeling:    
+        nummer = nummer + 1
+ #       print("flight z", z , " Aantal in die flight ", Dag + 1)
+        for lid in range(Grote):
+#            print("Dag: ", Dag ,"flights", z , " Aantal in die flight ", x, "nummer ", nummer)
+            WieOhWie(Dag,lid,nummer,Grote)
+print(FlightIndeling)
 
 
 
