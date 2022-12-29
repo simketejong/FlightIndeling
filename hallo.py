4e2277bb612b82b8fc9d7952a514dc3300872634
simketejong <simketejong@gmail.com>
2022-12-29 20:41:01 +0100

    Reperatie tav dictionary append

--- hallo.py
+++ hallo.py
@@ -13,7 +13,52 @@ TestDevide = TestRemain / MinimaleFlightGrote
 
 FlightVerdeling=[4,4,3,3] # testing
 
-J = { 
+def AppendFlight():
+    FlightIndeling.append({
+        "Dag" : 0,
+        "Flight" : 0,
+        "Grote" : 0,
+        "Personen" : []
+    })
+
+def WieOhWie(Dag,lid,nummer, Grote):
+    Dag=Dag+1
+    print(Dag,lid,nummer,Grote)
+    print("size :   " , len(FlightIndeling))
+    if len(FlightIndeling) == Dag:
+        AppendFlight()
+        FlightIndeling[Dag].update({"Dag": Dag})
+        FlightIndeling[Dag].update({"Flight": nummer})
+        FlightIndeling[Dag].update({"Grote": Grote})
+    GaDoor = True
+    Tijd = 1001
+    while GaDoor:
+        Kandidaat = random.randint(0, 13)            
+        if Dag not in Persoon[Kandidaat]["WelkeDag"]:
+            Tijd = Tijd - 1
+            if Tijd <= 0:
+                GaDoor = False
+                print("Kandidaat : ", Kandidaat, "niet gelukt in " , FlightIndeling[Dag])
+            if Kandidaat not in Persoon[Kandidaat]["met_wie_gespeeld"]:
+                Persoon[Kandidaat]["met_wie_gespeeld"].append(Kandidaat)
+                Persoon[Kandidaat]["WelkeFlight"].append([nummer])
+                Persoon[Kandidaat]["WelkeDag"].append([Dag])
+                GaDoor = False
+                #print(Persoon)
+            if Kandidaat not in FlightIndeling[Dag]["Personen"]:
+                FlightIndeling[Dag]["Personen"].append(Kandidaat)
+        else:
+            print("Kandidaat : ", Kandidaat, "speelt al op die dag" , Dag)           
+    print(Dag,lid,nummer,Grote,Kandidaat)        
+
+
+FlightIndeling.append({
+    "Dag" : 0,
+    "Flight" : 0,
+    "Grote" : 0,
+    "Personen" : []
+})
+Persoon.append({ 
     "nummer" : 1,
     "naam" : "",
     "hcp" : "",
@@ -21,48 +66,17 @@ J = {
     "met_wie_gespeeld" : [],
     "WelkeFlight" : [],
     "WelkeDag" : []
-}
-
-P = {
-    "Dag" : 0,
-    "Flight" : 0,
-    "Grote" : 0,
-    "Personen" : []
-}
-
-
-def WieOhWie(Dag,lid,nummer, Grote):
-    Dag=Dag+1
-    print(Dag,lid,nummer,Grote)
-    FlightIndeling.append(P)
-    FlightIndeling[Dag].update({"Dag": Dag})
-    FlightIndeling[Dag].update({"Flight": nummer})
-    FlightIndeling[Dag].update({"Grote": Grote})
-    GaDoor = True
-    Tijd = 1001
-    while GaDoor:
-        Kandidaat = random.randint(0, 13)            
-        if not Dag in Persoon[Kandidaat]["WelkeDag"]:
-            Tijd = Tijd - 1
-            if Tijd <= 0:
-                GaDoor = False
-                print("Kandidaat : ", Kandidaat, "niet gelukt in " , FlightIndeling[Dag])
-            if not Kandidaat in Persoon[Kandidaat]["met_wie_gespeeld"]:
-                Persoon[Kandidaat]["met_wie_gespeeld"].append(Kandidaat)
-                Persoon[Kandidaat]["WelkeFlight"].append([nummer])
-                Persoon[Kandidaat]["WelkeDag"].append([Dag])
-                GaDoor = False
-                #print(Persoon)
-            if not Kandidaat in FlightIndeling[Dag]["Personen"]:
-                FlightIndeling[Dag]["Personen"].append(Kandidaat)
-        #print("Kandidaat : ",Kandidaat)
-    print(Dag,lid,nummer,Grote,Kandidaat)        
-
-
-FlightIndeling.append(P)
-Persoon.append(J)
+})
 for x in range(AantalPersonen):
-    Persoon.append(J)
+    Persoon.append({ 
+    "nummer" : 1,
+    "naam" : "",
+    "hcp" : "",
+    "email" : "",
+    "met_wie_gespeeld" : [],
+    "WelkeFlight" : [],
+    "WelkeDag" : []
+})
     Persoon[x].update({"nummer": x})
     Persoon[x].update({"naam": x+1})
 #    print("Persoon = ",Persoon[x])
@@ -75,7 +89,7 @@ for Dag in range(AantalSpeelDagen ):
         for lid in range(Grote):
 #            print("Dag: ", Dag ,"flights", z , " Aantal in die flight ", x, "nummer ", nummer)
             WieOhWie(Dag,lid,nummer,Grote)
-#print(FlightIndeling)
+print(FlightIndeling)
 
 
 
---
+++ testDict.py
@@ -0,0 +1,17 @@
+
+S= {
+    "naam" : "naam",
+    "age" : 0
+}
+
+wie = []
+
+wie.append({    "naam" : "naam",
+    "age" : 0
+})
+wie.append({    "naam" : "naam",
+    "age" : 0
+})
+
+wie[0]["naam"]  = "simke"
+wie[0].update({"age": 53})   
